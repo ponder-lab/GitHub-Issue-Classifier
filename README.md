abb02a96cd17dc45ee8665b6910de5d7d74d590a
Y <ye.paing89@myhunter.cuny.edu>
2021-03-06 13:32:24 -0500

    feat: adding command line arguments (#34)
    feat: adding option to run script directly from command line

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Python script to mine for GitHub issues + comments and classify them.
 
 ![CLI Tool Screenshot](./cli_screenshot.png)
 
-### Setup:
+## Setup:
 1) Ensure Python `3.9.1` and corrosponding pip `3.9` are installed
 2) Install requirements: `pip3.9 install -r requirements.txt`
    - Part of the script also relies on downloading stop word dictionaries.
@@ -15,18 +15,54 @@ Python script to mine for GitHub issues + comments and classify them.
    
 Instructions on how to create a GitHub Personal Access Token: https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
  
-### Run:
-Run the `main.py` script: `python3.9 main.py` to start up the interface.
+## Run Instructions:
 
-### Test
+There are two ways to run this program, either through the interactive command line, or by directly passing in command line argument via `argparser`:
+
+Below is the `-h` help/man page:
+```aidl
+usage: mine-issues.py [-h] [-i] [-v] [-m MAX_RESULTS] [-s SORT_BY] [-p PREFIX_FILENAME] query
+
+positional arguments:
+  query
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i, --interface       **toggle the interactive CLI**
+  -v, --verbose         print additional logs
+  -m MAX_RESULTS, --max-results MAX_RESULTS
+                        (int) max results to query
+  -s SORT_BY, --sort-by SORT_BY
+                        sort by one of: [comments, best-match]
+  -p PREFIX_FILENAME, --prefix-filename PREFIX_FILENAME
+                        (string) file name prefix for result output files
+```
+
+### Usage:
+Run `python3.9 mine-issues.py <QUERY>` with the following optional parameters:
+
+`-i` or `--interface`: will cause the script to trigger the interactive CLI see in the screenshot above.
+
+`-v` or `--verbose`: will print out extra logging such as printing out the entire result object in neat JSON format.
+
+`-m` or `--max-results`: filter the number of results we want to retrieve from the search query (1000 by default).
+
+`-s` or `--sort-by`: Pick either `comments` or `best-match` to sort the search query result by (`comments` by default).
+
+`-p` or `--prefix-filename`: String to prefix to the resulting file name (`results_` by default).
+
+## Testing
+Running tests to ensure that the script is functioning properly. Travis CI build also runs this as part of build status checks.
 1) Ensure that `pytest` is properly set up for your python `3.9` env.
 2) Simply run `pytest` to check that all tests are passing.
 
-### Folder Structure
-`/utils` - Contains utility function files, such as IO, filtering results and processing comments
+## Folder Structure
+`/models` - Contains the serialized files of the classification model.
 
-`/test` - Contains test file being ran by `pytest`
+`/utils` - Contains utility function files, such as IO, filtering results and processing comments.
 
-`/result` - Folder to output result files to. Contains a `.gitignore` to ignore all files in this folder to prevent results from being committed
+`/test` - Contains test file being ran by `pytest`.
+
+`/result` - Folder to output result files to. Contains a `.gitignore` to ignore all files in this folder to prevent results from being committed.
 
 `/config` - Contains configuration files for the app to run, such as the personal access token.
--- mine-issues.py
+++ mine-issues.py
@@ -1,14 +1,4 @@
-'''
-GitHub issue mining script
-- Searches for issues using the search query API.
-- Filters out pull_request.
-- Splits up each comments of each issues by new line.
-- Store the lines as (issueID, line) tuple in an array.
-
-TODO:
-- Clean up comment line, process <CODE> tags properly.
-- Feed processed comment lines into linear regression model to predict comment types.
-'''
+import argparse
 
 from math import ceil
 from urllib.parse import urlencode
@@ -17,29 +7,62 @@ from joblib import load
 from utils.commentProcessor import processComment
 from utils.filterResults import filterIssueWithQueryString
 from utils.io import printJSON, writeResultToCSV
-from utils.githubAPI import gitHubSearchQueryAPI, gitHubCommentAPI
+from utils.githubAPI import gitHubSearchQueryAPI, gitHubCommentAPI, loadAccessToken
 
 from interface import InitializeSearchInterface
 
-'''
-GitHub limits search query to 1000 results at 100 results per page.
-However, we are also querying for all the comments for each of the results we get back.
+#
+# 0)
+# Setup argparser to figure out if user wants to use pyinquirer
+# or to just pass in the args via argparser
+#
+def sort_by_arg_checker(arg_value):
+	valid_options = ["comments", "best-match"]
 
-We should look to limiting our search results so that we don't get a 403 response on our
-additional queries for comments.
+	if arg_value not in valid_options:
+		raise argparse.ArgumentTypeError
 
-TODO: Implement authenticated API calls
-https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
+	return arg_value
+parser = argparse.ArgumentParser()
 
-Creating Personal Access Token:
-https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
-'''
+# Interface option, if true we toggle the interface below.
+# If false, we just grab the params from argparser result.
+parser.add_argument('-i', '--interface', action='store_true',
+					help="**toggle the interactive CLI**")
+
+parser.add_argument('query')
+
+parser.add_argument('-v', '--verbose', action='store_true',
+					help="print additional logs")
+parser.add_argument('-m', '--max-results',
+					type=int,
+					default=1000,
+					help="(int) max results to query")
+parser.add_argument('-s', '--sort-by',
+					type=sort_by_arg_checker,
+					default='comments',
+					help='sort by one of: [comments, best-match]')
+parser.add_argument('-p', '--prefix-filename',
+					default="results_",
+					help='(string) file name prefix for result output files')
+args = parser.parse_args()
 
 #
 # 1)
 # Initialize CLI/Get params
 #
-params = InitializeSearchInterface()
+
+params = {}
+
+# If user entered the '-i' or '--interface' option, trigger the pyinquirer interactive interface.
+if args.interface:
+	params = InitializeSearchInterface(args.query)
+else:
+	params['q'] = args.query
+	params['max_results'] = args.max_results
+	params['sort_by'] = args.sort_by
+	params['print_logs'] = args.verbose
+	params['out_file_prefix'] = args.prefix_filename
 
 PAGE = 1
 
@@ -54,6 +77,8 @@ SORT_BY = params['sort_by']
 PRINT_LOGS = params['print_logs']
 OUTPUT_FILE_PREFIX = params['out_file_prefix']
 
+# Load GitHub personal access token into memory.
+loadAccessToken()
 
 #
 # 2)
@@ -111,6 +136,11 @@ This uses a search_sample.json file results for testing/dev purpose
 ### DEV BLOCK END
 '''
 
+#
+# 3)
+# Omit any search results that whose body/title does not contain our search query
+# Checks for substring match
+#
 print(str(len(searchResults)) + " results retrieved.")
 
 # Filter out search results that does not contain our query in the body/title
@@ -132,11 +162,24 @@ if PRINT_LOGS:
 	print('\n OMITTED ISSUES \n')
 	printJSON(OMITTED_ISSUES)
 
+'''
+### DEV BLOCK START
+This slices the first 10 matches so we don't overload GitHub's API limit
+'''
 # Dev code to limit results to be processed to not further overload the API
 # MATCHED_RESULTS = MATCHED_RESULTS[0:10]
 
 # Test comment API URL:
 # https://api.github.com/repos/tensorflow/tensorflow/issues/27880/comments
+'''
+### DEV BLOCK END
+'''
+
+#
+# 4)
+# For each of the results (MATCHED_RESULTS) that matches our query
+# Fetch their comments and clean/tokenize them.
+#
 
 # Hold all comment lines, and tag each individual lines with the comment/issue related data
 # i.e URLs, IDs, links etc...
@@ -173,10 +216,15 @@ print("\n")
 # and add them to the corpus.
 CORPUS += gitHubCommentAPI(issues_api_urls)
 
+#
+# 5)
+# Once we have fetch all the comments, and split them up into lines and tokenized them
+# Load the prediction model from the serialized file and predict the comments.
+#
 ### Load Model/Vector - Use the serialized model/count vector files included
-model = load("GitHub_comments_logisticRegression.model")
+model = load("models/GitHub_comments_logisticRegression.model")
 
-vectorizer = load("GitHub_comments_logisticRegression.countVector")
+vectorizer = load("models/GitHub_comments_logisticRegression.countVector")
 
 for c in CORPUS:
 	test_vector = vectorizer.transform([c['commentLine']])
